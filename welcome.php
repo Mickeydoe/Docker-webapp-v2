c7ec5ea64b56a9d820740d2ab6a05f151b5a447e
Mickeydoe <michaeldanquah615@gmail.com>
2024-06-13 21:36:42 +0000

    new commits

--- welcome.php
+++ welcome.php
@@ -8,9 +8,12 @@ $username = htmlspecialchars($_GET['username']);
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Welcome</title>
+    <link rel="stylesheet" href="styles.css">
 </head>
 <body>
-    <h1>Welcome, <?php echo $username; ?>!</h1>
-    <p>You have successfully logged in.</p>
+    <div class="container">
+        <h1>Welcome, <?php echo $username; ?>!</h1>
+        <p>You have successfully logged in.</p>
+    </div>
 </body>
 </html>
